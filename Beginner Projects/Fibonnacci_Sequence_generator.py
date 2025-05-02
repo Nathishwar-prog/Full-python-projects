e7138ce8c4518205ea769a3b15aae0f484060fc3
Nathishwar <182191081+Nathishwar-prog@users.noreply.github.com>
2025-05-02 19:43:59 +0530

    Create Fibonnacci_Sequence_generator.py

---
+++ Beginner Projects/Fibonnacci_Sequence_generator.py
@@ -0,0 +1,10 @@
+def fibonacci():
+    print("Fibonacci Sequence Generator")
+    n = int(input("How many terms? "))
+    
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+fibonacci()
